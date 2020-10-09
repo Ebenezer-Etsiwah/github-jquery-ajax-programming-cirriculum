55cd20d76da9c65ae8b00c97d401e8cacdcfa2cf
Ebenezer <ETSIWAH@GMAIL.COM>
2020-10-09 13:03:23 +0000

    To use the first,next,last and prev command

---
+++ 8.First,Next,Last and Prev/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+ console.log($('#container)
+ .find('.hot')
+ .children()
+ .last()
+ .prev()
+ //.first()
+//.next() used to highlight the second item on the list.
+
+ .addClass('highlight');
+);
+});
